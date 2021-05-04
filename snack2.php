9dc65d365692a34972e8a8ab0df91e17e2d01c23
Gabriele Alessi <89archaon89@gmail.com>
2021-05-04 13:56:30 +0200

    Esercizio 2

---
+++ snack2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+
+         <?php
+
+            $name = $_GET['name'];
+            $nameLng = strlen ( $name );
+            $mail = $_GET['email'];
+            $validMail = false;
+            $age = $_GET['age'];
+            $validAge = is_numeric ($age);
+
+            //verifico che l'email contenga il punto e la chiocciola
+            if (!strpos ( $mail , '.')===false && !strpos ( $mail , '@')===false) {
+                $validMail = true;
+            }
+
+            //verifico che tutti i parametri siano rispettati
+            if ($nameLng > 3 && $validMail && $validAge) {
+                echo "Accesso consentito";
+            }else {
+                echo "Accesso negato";
+            }
+          ?>
+    </body>
+</html>
